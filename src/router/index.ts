0b054812ef62cf1bbd6a5d0204c4479360608352
Pcjmy <1716045191@qq.com>
2023-07-09 15:47:46 +0800

    feat: 登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
 import Home from '@/pages/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  // {
-  //   path: '/',
-  //   name: 'login',
-  // },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
   {
     path: '/',
     name: 'home',
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,83 @@
 <template>
-  <h1>Login</h1>
+  <div class="container">
+    <el-form ref="formRef" :model="dynamicValidateForm" label-width="120px" class="demo-dynamic">
+      <el-form-item
+        prop="username"
+        label="username"
+        :rules="[
+          {
+            required: true
+          }
+        ]"
+      >
+        <el-input v-model="dynamicValidateForm.username" />
+      </el-form-item>
+      <el-form-item
+        prop="password"
+        label="password"
+        :rules="[
+          {
+            required: true
+          }
+        ]"
+      >
+        <el-input type="password" v-model="dynamicValidateForm.password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          style="width: 100%; margin-bottom: 30px"
+          @click="submitForm(formRef)"
+          >登录</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormInstance } from 'element-plus'
+
+interface DomainItem {
+  key: number
+  value: string
+}
+
+const formRef = ref<FormInstance>()
+const dynamicValidateForm = reactive<{
+  domains: DomainItem[]
+  username: string
+  password: string
+}>({
+  domains: [
+    {
+      key: 1,
+      value: ''
+    }
+  ],
+  username: '',
+  password: ''
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!')
+      return false
+    }
+  })
+}
+</script>
+
+<style lang="scss">
+.container {
+  width: 500px;
+  position: absolute;
+  left: 30%;
+  top: 30%;
+}
+</style>
